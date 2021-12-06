3401390d3c01c72f41d20d527ddcf51eda1a5c48
Marcos Biagi <40881833+marcosbdev@users.noreply.github.com>
2021-12-06 00:05:20 -0300

    Poner piedras simple

---
+++ Assets/RockData.cs
@@ -0,0 +1,20 @@
+using System;using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Rocks/Rock")]
+public class RockData : ScriptableObject
+{
+    public string name;
+    public rarities rarity;
+    public float price;
+}
+
+public enum rarities
+{
+    normal,
+    epic,
+    mega,
+    legendary,
+    gamer
+};
--- Assets/RockPlacer.cs
+++ Assets/RockPlacer.cs
@@ -1,18 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RockPlacer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject selected_rock;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private GameObject rocksParent;
+
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 worldPosition2D = new Vector2(worldPosition.x, worldPosition.y);
+            var randomRotation = Quaternion.Euler( 0 , 0 , Random.Range(0, 360));
+            GameObject newRock = Instantiate(selected_rock, worldPosition2D, randomRotation, rocksParent.transform); //set as type rock?
+        }
     }
 }
