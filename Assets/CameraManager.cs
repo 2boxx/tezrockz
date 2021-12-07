800d64e778b221645bff07c89b572794cee2e7f0
Marcos Biagi <40881833+marcosbdev@users.noreply.github.com>
2021-12-07 02:43:47 -0300

    arreglos a la camara

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -7,10 +7,18 @@ public class CameraManager : MonoBehaviour
 {
     [SerializeField] private RocksManager _rocksManager;
     [SerializeField] private GameObject cameraTarget;
+    public float minimumY = 0f;
 
     private void LateUpdate()
     {
-        if (_rocksManager.GetHighestRock()!=null)
-            cameraTarget.transform.position = _rocksManager.GetHighestRock().gameObject.transform.position;
+        
+        if (_rocksManager.GetHighestRock() == null) return;
+        Transform highestRockTransform = _rocksManager.GetHighestRock().gameObject.transform;
+        if (highestRockTransform.position.y < minimumY) return;
+        cameraTarget.transform.position =
+            new Vector3(
+                cameraTarget.transform.position.x,
+                highestRockTransform.position.y,
+                cameraTarget.transform.position.z);
     }
 }
