c9d0579dbd00e8ef11fc80fbda510bdb06d4fa89
Marcos Biagi <40881833+marcosbdev@users.noreply.github.com>
2021-12-16 03:10:35 -0300

    netezos import

--- Assets/Scripts/RockPlacer.cs
+++ Assets/Scripts/RockPlacer.cs
@@ -75,7 +75,7 @@ public class RockPlacer : MonoBehaviour
         }
 ;
         //Store a rotation and update it based on mousewheel...
-//        Debug.Log("Input.GetAxis(Mouse ScrollWheel) = " + Input.GetAxis("Mouse ScrollWheel"));
+        //        Debug.Log("Input.GetAxis(Mouse ScrollWheel) = " + Input.GetAxis("Mouse ScrollWheel"));
         eulerRot = eulerRot + (Input.GetAxis("Mouse ScrollWheel") + Input.GetAxis("Horizontal")) * rotSpeed * Time.deltaTime;
         previewRotation =  quaternion.Euler(0f, 0f, eulerRot);
        
