6cd5f1e38e1cc4763fb20b12efd55bf8cc86d038
Nicolas Ferreiro <41026986+srezu@users.noreply.github.com>
2021-12-16 05:12:28 -0300

    Rocks joints update

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -7,4 +7,48 @@ using UnityEngine;
 public class Rock : MonoBehaviour
 {
     public RockData rockData;
+    SpriteRenderer _rend;
+
+    private Rigidbody2D _rb;
+
+    public bool hasJoint;
+    
+    void Start()
+    {
+        _rend = GetComponent<SpriteRenderer>();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (_rend.isVisible)
+        {
+            _rb.isKinematic = false;
+        }
+        else
+        {
+            _rb.isKinematic = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.CompareTag("Rock"))
+        { 
+            if (hasJoint) return;
+            
+            if (other.gameObject.GetComponent<Rigidbody2D>())
+            {
+                hasJoint = true;
+                HingeJoint2D  joint = gameObject.AddComponent(typeof(HingeJoint2D )) as HingeJoint2D ;
+                joint.connectedBody = other.gameObject.GetComponent<Rigidbody2D>();
+                joint.enableCollision = true;
+                joint.breakForce = 150f;
+     
+
+            }
+              
+        }
+    }
 }
